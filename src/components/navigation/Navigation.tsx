b79df998544e6f2b5cffd4b9a1e12f50e7e29afa
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-05-04 21:33:05 +0200

    feat(navigation): added custom logo depending on dark/light mode

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { ScrollProgress } from "../magicui/scroll-progress";
+import { ModeToggle } from "@/components/ModeToggle";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import DarkLogo from "../../../public/logo-dark-mode.png";
+import LightLogo from "../../../public/logo-light-mode.png";
+import Link from "next/link";
+
 export const Navigation = () => {
-    return (
-        <nav className="flex items-center justify-center">
-            
-        </nav>
-    )
-}
+  const { theme, resolvedTheme } = useTheme();
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    if (resolvedTheme === "dark") {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+  }, [resolvedTheme]);
+
+  return (
+    <nav className="w-full py-8 fixed bg-background">
+      <ScrollProgress className="bottom-100 fixed" />
+      <ul className="w-[1240px] mx-auto flex gap-8 items-center">
+        <Image src={isDark ? DarkLogo : LightLogo} alt="Logo" width={28} />
+        <Link href="#showcase">Showcase</Link>
+        <Link href="#skills">Skills</Link>
+        <div className="ml-auto">
+          <ModeToggle />
+        </div>
+      </ul>
+    </nav>
+  );
+};
