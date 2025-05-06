76766bf9d4e04b189d3812cd01225d534ac19fbd
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-05-06 22:48:05 +0200

    feat(showcase): added utils file with interface for projects export

---
+++ src/lib/projects.ts
@@ -0,0 +1,85 @@
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    github: string;
+    live: string;
+    techstack: string[];
+    keyImage: string
+  }
+
+  export const projects: Project[] = [
+    {
+        id: 0,
+        name: "Manga API", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/portfolio-starter-JoachimHamraoui", 
+        live: "", 
+        techstack: ["NodeJS", "Docker", "React", "PostgreSQL"],
+        keyImage: "/showcase/manga-app.png"
+    },
+    {
+        id: 1,
+        name: "Chat App", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/chat-app", 
+        live: "", 
+        techstack: ["NodeJS", "Websockets", "React"],
+        keyImage: "/showcase/chat-app.png"
+    },
+    {
+        id: 2,
+        name: "Manga API", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/portfolio-starter-JoachimHamraoui", 
+        live: "", 
+        techstack: ["NodeJS", "Docker", "React", "PostgreSQL"],
+        keyImage: "/showcase/manga-app.png"
+    },
+    {
+        id: 3,
+        name: "Chat App", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/chat-app", 
+        live: "", 
+        techstack: ["NodeJS", "Websockets", "React"],
+        keyImage: "/showcase/chat-app.png"
+    },
+    {
+        id: 4,
+        name: "Manga API", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/portfolio-starter-JoachimHamraoui", 
+        live: "", 
+        techstack: ["NodeJS", "Docker", "React", "PostgreSQL"],
+        keyImage: "/showcase/manga-app.png"
+    },
+    {
+        id: 5,
+        name: "Chat App", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/chat-app", 
+        live: "", 
+        techstack: ["NodeJS", "Websockets", "React"],
+        keyImage: "/showcase/chat-app.png"
+    },
+    {
+        id: 6,
+        name: "Manga API", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/portfolio-starter-JoachimHamraoui", 
+        live: "", 
+        techstack: ["NodeJS", "Docker", "React", "PostgreSQL"],
+        keyImage: "/showcase/manga-app.png"
+    },
+    {
+        id: 7,
+        name: "Chat App", 
+        description: "Docker, NodeJS, API", 
+        github: "https://github.com/JoachimHamraoui/chat-app", 
+        live: "", 
+        techstack: ["NodeJS", "Websockets", "React"],
+        keyImage: "/showcase/chat-app.png"
+    },
+    
+  ]
