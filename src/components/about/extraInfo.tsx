799bfc7c5ce162b2fcae142fbe8e35eb3c2e15f2
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-05-21 10:56:53 +0200

    feat(about): add inView animation for the text in tab 1

--- src/components/about/extraInfo.tsx
+++ src/components/about/extraInfo.tsx
@@ -1,25 +1,40 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+import { fadeUpVariant } from "@/lib/motionVariants";
+
 export const ExtraInfo = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
   const birthDate = new Date(1999, 0, 29); // Month is 0-indexed: 0 = January
   const today = new Date();
 
   const age = today.getFullYear() - birthDate.getFullYear();
   const hasHadBirthdayThisYear =
     today.getMonth() > birthDate.getMonth() ||
-    (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+    (today.getMonth() === birthDate.getMonth() &&
+      today.getDate() >= birthDate.getDate());
 
   const finalAge = hasHadBirthdayThisYear ? age : age - 1;
 
   return (
-    <div className="w-2/3 flex flex-col my-12">
-      <p className="text-4xl text-primary ">
-        I&apos;m a {finalAge} year old <span className="font-semibold">Fullstack Web Developer</span> based in <span className="font-semibold">Brussels, Belgium</span>. I
-        recently graduated with a <br />Bachelor&apos;s Degree at Erasmushogeschool Brussel.<br /> I&apos;m fluent in French, Dutch and English. 
+    <div ref={ref} className="w-2/3 flex flex-col my-12">
+      <motion.p
+        className="text-4xl text-primary "
+        variants={fadeUpVariant}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+      >
+        I&apos;m a {finalAge} year old{" "}
+        <span className="font-semibold">Fullstack Web Developer</span> based in{" "}
+        <span className="font-semibold">Brussels, Belgium</span>. I recently
+        graduated with a <br />
+        Bachelor&apos;s Degree at Erasmushogeschool Brussel.
+        <br /> I&apos;m fluent in French, Dutch and English.
         <br />
         <br />
-        Eager to learn more
-        about Web & App Development. I also read, game and do graphic design in
-        my free time.
-      </p>
+        Eager to learn more about Web & App Development. I also read, game and
+        do graphic design in my free time.
+      </motion.p>
     </div>
   );
 };
