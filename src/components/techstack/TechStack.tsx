bbe35f5230761d556804e5520bda3b188fef98e2
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-05-21 18:11:12 +0200

    refactor: changed tech stack to just a flex wrap component with inview animation

--- src/components/techstack/TechStack.tsx
+++ src/components/techstack/TechStack.tsx
@@ -1,14 +1,21 @@
-import { Marquee } from "../magicui/marquee";
+"use client";
+
 import Image from "next/image";
 
+import { fadeUpVariant } from "@/lib/motionVariants";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
 export const TechStack = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const techstack = [
     "JavaScript",
     "TypeScript",
     "React",
-    "Vue.js",
-    "Vite",
-    "Webpack",
+    "Vue.JS",
+    "Next.JS",
     "Git",
     "PHP",
     "Laravel",
@@ -22,41 +29,39 @@ export const TechStack = () => {
     "CSS",
     "Tailwindcss",
     "Kotlin",
-    "Drupal",
-    "Expo"
   ];
 
-  const firstHalf = techstack.slice(0, techstack.length / 2);
-  const secondHalf = techstack.slice(techstack.length / 2);
-
   return (
-    <div className="w-full mt-24">
-      <Marquee pauseOnHover>
-        <div className="flex flex-row items-center gap-24 px-12">
-          {firstHalf.map((item) => (
+    <div className="w-[1240px] mx-auto flex flex-col mt-24">
+      <motion.h2
+        className="text-4xl font-bold"
+        variants={fadeUpVariant}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+      >
+        Skills
+      </motion.h2>
+      <div
+        ref={ref}
+        className="w-full my-12 grid grid-cols-6 gap-4 gap-y-16"
+      >
+        {techstack.map((item) => (
+          <motion.div
+            className="flex items-center justify-space-between"
+            key={item}
+            variants={fadeUpVariant}
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+          >
             <Image
-              key={item}
-              src={`https://cdn.simpleicons.org/${item}/grey`}
+              src={`https://cdn.simpleicons.org/${item}`}
               alt={item}
               width={100}
               height={100}
             />
-          ))}
-        </div>
-      </Marquee>
-      <Marquee pauseOnHover reverse className="mt-8">
-        <div className="flex flex-row items-center gap-24 px-12">
-          {secondHalf.map((item) => (
-            <Image
-              key={item}
-              src={`https://cdn.simpleicons.org/${item}/grey`}
-              alt={item}
-              width={100}
-              height={100}
-            />
-          ))}
-        </div>
-      </Marquee>
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 };
