5710e9e8e37198a257c550f0b905824d54a5b61e
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-06-04 08:03:38 +0200

    feat(cursor): added a custom cursor with a cursor context to use anywhere

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import { Navigation } from "@/components/navigation/Navigation";
-import { SmoothCursor } from "@/components/ui/smooth-cursor";
 import { Hero } from "@/components/hero/Hero";
 import { Showcase } from "@/components/showcase/Showcase";
 import { About } from "@/components/about/About";
 import { TechStack } from "@/components/techstack/TechStack";
 import { Experiences } from "@/components/Experiences/Experiences";
+// import { Footer } from "@/components/footer/Footer";
+import { CursorProvider } from "@/context/CursorContext";
+import { SmoothCursor } from "@/components/cursor/SmoothCursor";
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center">
-      <Navigation />
-      <Hero />
-      <Showcase />
-      <TechStack />
-      <About />
-      <Experiences />
-    </div>
+    <CursorProvider>
+      <div className="w-full flex flex-col items-center cursor-none">
+        <SmoothCursor
+          cursor={
+            <div className="w-6 h-6 rounded-full backdrop-invert bg-black/10 dark:bg-white/10" />
+          }
+        />
+        <Navigation />
+        <Hero />
+        <Showcase />
+        <TechStack />
+        <About />
+        <Experiences />
+        {/* <Footer /> */}
+      </div>
+    </CursorProvider>
   );
 }
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,6 +7,10 @@ import { useTheme } from "next-themes";
 import { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
 import { motion, useInView } from "framer-motion";
+import { useCursor } from "@/context/CursorContext";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 // Dynamically import the client-only Lottie component
 const LottieClientOnly = dynamic(() => import("../clients/LottieClientOnly"), {
@@ -16,6 +20,8 @@ const LottieClientOnly = dynamic(() => import("../clients/LottieClientOnly"), {
 export const Hero = () => {
   const { resolvedTheme } = useTheme();
   const [isDark, setIsDark] = useState(false);
+
+  const { setHovered } = useCursor();
 
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -43,16 +49,31 @@ export const Hero = () => {
           initial="hidden"
           animate={isInView ? "visible" : "hidden"}
           className="inline-block" // ✅ important
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
         >
-          <AuroraText colors={["#f7ba2c", "#ea5459"]}>
-            Joachim
-          </AuroraText>
+          <AuroraText colors={["#f7ba2c", "#ea5459"]}>Joachim</AuroraText>
         </motion.span>
       </h1>
       <p className="text-xl">
         Fullstack Web Developer in Brussels — <br />
         Turning Ideas into User-Focused Digital Products
       </p>
+      <div className="mt-6 flex gap-4">
+        <Link
+          href="https://www.linkedin.com/in/joachim-hamraoui-6950b6173/"
+          target="_blank"
+        >
+          <Button>
+            <FaLinkedin /> Add me on LinkedIn
+          </Button>
+        </Link>
+        <Link href="https://github.com/JoachimHamraoui" target="_blank">
+          <Button variant="outline">
+            <FaGithub /> Check out my GitHub
+          </Button>
+        </Link>
+      </div>
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 w-6">
         <LottieClientOnly
           animationData={isDark ? DarkMouse : LightMouse}
